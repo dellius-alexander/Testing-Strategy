7c307e33c7026852ecefe5aa7a58dfaa1fd5cdaa
eerod94 <49722956+eerod94@users.noreply.github.com>
2020-10-29 22:39:21 -0400

    Create gallery_sizing2.spec.js

---
+++ cypress_tests/gallery_sizing2.spec.js
@@ -0,0 +1,14 @@
+describe("Some describe", () => {
+    before(() => {
+      cy.visit('https://dellius-alexander.github.io/responsive_web_design/gallery.html');
+    });
+
+    describe("Desktop", () => {
+      before(() => {
+        cy.viewport(1440, 900) // view for MacBook 15
+      })
+      it ('checks for size', () => {
+        cy.contains('Gallery')
+    })
+    });
+  });
