318ad7cd47b289d4e1e69511247cd073bdb2ea01
dellius-alexander <dellius.alexander@gmail.com>
2020-11-03 00:18:23 -0500

    removed qa_tests

--- qa_tests/demo.spec.js
+++
@@ -1,8 +0,0 @@
-//clicks on Project Plan Page
-describe('Hyfi Solutions', () => {
-    it('clicks the link "type"', () => {
-      cy.visit('https://dellius-alexander.github.io/responsive_web_design/index.html')
-  
-      cy.contains('Project Plan').click()
-         })
-  })
--- qa_tests/hyfi.spec.js
+++
@@ -1,9 +0,0 @@
-//verifies title page
-describe('Hyfi Solutions home page', function() {
-    beforeEach(function() {
-        cy.visit('https://dellius-alexander.github.io/responsive_web_design/index.html');
-      });
-    
-    it('contains "HYFI SOLUTIONS, Inc" in the title', function() {
-    });
-});
