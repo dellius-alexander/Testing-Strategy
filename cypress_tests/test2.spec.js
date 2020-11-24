22e5f81db782ebf6a2447effdb76431b07984110
* <*>
2020-11-24 17:49:47 -0500

    updated init file

--- cypress_tests/test2.spec.js
+++
@@ -1,6 +0,0 @@
-//goes to gallery page as homepage
-describe('Hyfi Solutions Test', () => {
-    it('Visits the Hyfi Solutions Gallery Page', () => {
-      cy.visit('gallery.html')
-    })
-  })
--- cypress_tests/test4.spec.js
+++
@@ -1,8 +0,0 @@
-//clicks on the about Us page
-describe('Hyfi Solutions', () => {
-    it('clicks the link "type"', () => {
-      cy.visit('index.html')
-  
-      cy.contains('About us').click()
-    })
-  })
