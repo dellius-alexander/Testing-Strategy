8d521e65ca3177e1d4a7f544295c8cbd21a18f75
* <*>
2020-11-24 17:49:47 -0500

    updated init file

--- cypress_tests/hyfi.spec.js
+++
@@ -1,9 +0,0 @@
-//verifies title page
-describe('Hyfi Solutions home page', function() {
-    beforeEach(function() {
-        cy.visit('index.html');
-      });
-    
-    it('contains "HYFI SOLUTIONS, Inc" in the title', function() {
-    });
-});
